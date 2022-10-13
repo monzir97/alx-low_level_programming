266473c89512ce13c4dcff0c0a551ee4ac4c4259
monzir97 <monzirsalih45@gmail.com>
2022-10-13 16:39:30 -0700

    mission 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main -  print the provided text
+ *
+ * Return: return zero
+ */
+int main(void)
+{
+char out[] = "Programming is like building a multilingual puzzle";
+puts(out);
+return (0);
+
+
+
+
+
+
+}
