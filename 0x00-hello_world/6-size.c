72b972e470531593bfc0e1199c8027808d908839
monzir97 <monzirsalih45@gmail.com>
2022-10-13 17:33:10 -0700

    finnaly

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void){
+printf("Size of a char: %lu byte(s)\n",sizeof(char));
+printf("Size of a int: %lu byte(s)\n",sizeof(int));
+printf("Size of a long: %lu byte(s)\n",sizeof());
+printf("Size of a long long int: %lu byte(s)\n",sizeof())
+printf("Size of a int: %lu byte(s)\n",sizeof(float));
+return 0;
+
+}
