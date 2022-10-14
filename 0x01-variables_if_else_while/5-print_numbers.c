dbe716960a8e836db59bc4afc76faad7993d032b
monzir97 <monzirsalih45@gmail.com>
2022-10-14 16:18:18 -0700

    zero

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main- print the numbers from one to ten
+ *
+ *  Return: return zero
+ */
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+printf("%d", i);
+i++;
+}
+printf("\n");
+}
