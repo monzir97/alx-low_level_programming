21428d4607f4c541e98545bc489f62bac93f14f4
monzir97 <monzirsalih45@gmail.com>
2022-10-14 16:08:33 -0700

    the first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main- prints all alphpet except e,q
+ *
+ * Return: it returns nothing
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+c++;
+}
+
+putchar('\n');
+}
