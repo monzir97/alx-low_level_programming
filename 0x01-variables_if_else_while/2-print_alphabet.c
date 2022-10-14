537b0473c167ba47f957e85fec3cbca16ccefab4
monzir97 <monzirsalih45@gmail.com>
2022-10-14 15:37:17 -0700

    send

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-
-int  main()
+/**
+ * main - go through the character
+ * Return: it return alphabet
+ */
+int  main(void)
 {
-char c ='a';
-while(c <= 'z')
+char c = 'a';
+while (c <= 'z')
 {
 putchar(c);
 c++;
