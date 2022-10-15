bedfef199678da287ebfa4cb19d0346c81baecaf
monzir97 <monzirsalih45@gmail.com>
2022-10-14 17:44:27 -0700

    the last one

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main- printting the numbers from one to nine
+ *
+ * Return: return zero
+ */
+int main(void)
+{
+int a = 0;
+
+while (a <= 9)
+{
+if (a < 9)
+{
+putchar(a + '0');
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar(a + '0');
+}
+a++;
+}
+return (0);
+}
