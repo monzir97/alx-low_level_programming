96a6e0e4731a5e978eb830b281980f928fdf41fb
monzir97 <monzirsalih45@gmail.com>
2022-10-19 16:29:30 -0700

    finall

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_islower- return zero the lower
+ *@c: the letter
+ * Return: zero for the upper one the lower
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+
+}
+
