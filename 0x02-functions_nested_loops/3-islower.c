82fa15fd1a9d7b4be6660ed09610d572587d2cb1
monzir97 <monzirsalih45@gmail.com>
2022-10-19 17:04:14 -0700

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *_islower- return zero the lower
- *@c: the letter
- * Return: zero for the upper one the lower
+*_islower- return zero the lower
+*@c: the letter
+* Return: zero for the upper one the lower
  */
 int _islower(int c)
 {
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "header.h"
+/**
+ *_abs-returns the abosulute value
+ *@n: the number to be chekced
+ *Return: it returns n or negative n
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
