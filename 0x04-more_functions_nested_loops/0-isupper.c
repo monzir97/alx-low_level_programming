8ad36b9e80445114653bacc96a567154b95addfd
monzir97 <monzirsalih45@gmail.com>
2022-10-21 11:53:25 -0700

    first one

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper- function determine whether character is upper or not
+ * @c:the character to be chekced
+ * Return: it return 1 for upper 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
